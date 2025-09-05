c32ae2bfe8be7d7938bb2d44b5feb7cc21e24926
Justin Alvey <jual6332@colorado.edu>
2025-09-05 19:15:07 -0400

    Created new file demo 4 script

---
+++ shellclass/localusers/luser-demo04.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This sceipt creates an account on the local system
+# You will be prompted for the account name and password.
+
+# Ask for the user name.
+
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set the password for the user.
+
+# Force password change on first login.
+
