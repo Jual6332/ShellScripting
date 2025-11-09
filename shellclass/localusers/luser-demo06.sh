185243f00a2e82fd0c71423e3f2c9e1feec2c77d
Justin Alvey <jual6332@colorado.edu>
2025-11-09 17:31:54 -0500

    Learned for loops and more

--- shellclass/localusers/luser-demo06.sh
+++ shellclass/localusers/luser-demo06.sh
@@ -13,3 +13,30 @@ path=$(dirname ${0})
 
 echo "Filename is: ${fileName}"
 echo "Directory path is ${path}"
+
+# How many arguments?
+NUMBER_OF_PARAMETERS="${#}"
+echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line"
+
+# Make sure that at least one argument is supplied.
+if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]
+then
+  echo "Usage: ${0} USER_NAME [USER_NAME]..."
+  exit 1
+fi
+
+# Implement for loop to display greeting to 3 people
+for X in Frank Claire Doug
+do
+  echo "${X}"
+done
+
+# Generate and display a password for each parameter
+for USER_NAME in "${@}"
+do
+  PASSWORD=$(date +%s%N | sha256sum | head -c48)
+  echo "${USER_NAME}: ${PASSWORD}"
+done
+
+# User can input 1,2,or 10 usernames and ${@} will handle it.
+
