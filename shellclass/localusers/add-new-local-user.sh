ece9056da9932c655aafac64223081e872789cc3
Justin Alvey <jual6332@colorado.edu>
2025-11-09 20:12:47 -0500

    Completed Section 4

--- shellclass/localusers/add-new-local-user.sh
+++ shellclass/localusers/add-new-local-user.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This script creates an account on the local system
+# This script creates a new user on the local system
+# You must supply a usernamme as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument as the second argument.
 
 # Make sure the script is being executed with superuser privileges.
 UID_TO_TEST_FOR='0'
@@ -17,29 +19,33 @@ echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line"
 # Make sure that at least one argument is supplied.
 if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]
 then
-  echo "Usage: ${0} USER_NAME [USER_NAME]..."
+  echo "Usage: ${0} USER_NAME [COMMENT]..."
+  echo 'Create an account on the local system with the namme of USER_NAME and a comments field of COMMENT.'
   exit 1
 fi
 
-# STore username in USER_NAE variable
+# Store username in USER_NAME variable
 USER_NAME=${1}
-if [[ "${NUMBER_OF_PARAMETERS}" -eq 2 ]]
-then
-  COMMENT=${2}
-else
-  COMMENT=""
-fi
+#if [[ "${NUMBER_OF_PARAMETERS}" -eq 2 ]]
+#then
+#  COMMENT=${2}
+#else
+#  COMMENT=""
+#fi
+shift
+COMMENT="${@}"
 
 # Automatically generates a password for the user:
 PASS=$(date +%s%N | sha256sum | head -c48)
 
 # Create the user.
 useradd -c "${COMMENT}" -m ${USER_NAME}
+# -m supplies a home directory for the user
 
 # Check to see if the useradd command succeeded.
 if [[ "${?}" -ne 0 ]]
 then
-  echo "The useradd command failed."
+  echo "The account could not be created."
   exit 1
 fi
 
@@ -49,7 +55,7 @@ echo ${PASS} | passwd --stdin ${USER_NAME}
 # Check to see if the passwd command succeeded.
 if [[ "${?}" -ne 0 ]]
 then
-  echo "The passwd command failed."
+  echo "The password could not be set."
   exit 1
 fi
 
@@ -57,10 +63,13 @@ fi
 passwd -e ${USER_NAME}
 
 # Display the username, password, and the host where the user was created.
+echo
 echo "username:"
 echo "${USER_NAME}"
+echo
 echo "password:"
 echo "${PASS}"
+echo
 echo "host:"
 command=$(hostname -f)
 echo "${command}"
