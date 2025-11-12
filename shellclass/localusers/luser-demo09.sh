6cdbf3acaede958e81cbca19ac15af43dde9ea53
Justin Alvey <jual6332@colorado.edu>
2025-11-11 19:16:15 -0500

    LEarned about case statements and how they are better than if-else blocks

---
+++ shellclass/localusers/luser-demo09.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script demonstrates the case statement.
+
+#if [[ "${1}" = 'start' ]]
+#then 
+#  echo 'Starting.'
+#elif [[ "${1}" = 'stop' ]]
+#then
+#  echo 'Stopping.'
+#elif [[ "${1}" = 'status' ]]
+#then
+#  echo 'Status:'
+#else
+#  echo 'Supply a valid option' >&2
+#  exit 1
+#fi
+
+case "${1}" in
+  start)
+    echo 'Starting.'
+    ;;
+  stop)
+    echo 'Stopping.'
+    ;;
+  status|state|--state|--status)
+    echo 'Status:'
+    ;;
+  *)
+    echo 'Supply a valid option' >&2
+    exit 1
+    ;;
+esac
+
+# Cleaner, requires less code because we don't have to repeat ourselves over and over like in the if statements above.
+
+
