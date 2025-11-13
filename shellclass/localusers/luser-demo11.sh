a44b973ef392becb7f43f40657efaeddc33bea5f
Justin Alvey <jual6332@colorado.edu>
2025-11-13 18:00:25 -0500

    Learning getopts

---
+++ shellclass/localusers/luser-demo11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script generates a random password.
+# This user can set he password length with -l and add a special character with -s.
+# Verbose mode can be enabled with -v.
+
+# Set default password length
+LENGTH=48
+
