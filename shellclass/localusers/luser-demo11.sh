3e4a77a4ce2d07489600cf22811150e0c6bc9a8d
Justin Alvey <jual6332@colorado.edu>
2025-11-26 14:48:43 -0500

    Added new flag inputs via getopts builtin

--- shellclass/localusers/luser-demo11.sh
+++ shellclass/localusers/luser-demo11.sh
@@ -7,3 +7,62 @@
 # Set default password length
 LENGTH=48
 
+
+usage(){
+  echo "Usage: ${0} [-vs][-l LENGTH]" >&2
+  echo 'Generate a random password.'
+  echo '  -l LENGTH  Specify the password length.'
+  echo '  -s         Append a special character to the password.'
+  echo '  -v         Increase verbosity.'
+  exit 1
+}
+
+log() {
+  local MESSAGE="${@}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+    echo "${MESSAGE}"
+  fi
+
+}
+
+while getopts vl:s OPTION
+do
+  case ${OPTION} in
+    v)
+      VERBOSE='true'
+      log 'Verbose mode on.'
+      ;;
+    l)
+      LENGTH="${OPTARG}"
+      ;;
+    s)
+      USE_SPECIAL_CHARACTER='true'
+      ;;
+    ?)
+      #echo 'Invalid option.' >&2
+      usage
+      exit 1
+      ;;
+   esac
+done
+
+log 'Generating a password.'
+
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LENGTH})
+
+# Append a special character if requested to do so.
+if [[ ${USE_SPECIAL_CHARACTER} = 'true' ]]
+then
+  log 'Selecting a random special character.'
+  SPECIAL_CHARACTER=$(echo '!@#$%^&*()-+=' | fold -w1 | shuf | head -c1)
+  PASSWORD="${PASSWORD}${SPECIAL_CHARACTER}"
+fi
+
+log 'Done.'
+log 'here is the password:'
+
+# Display the password (even if verbose mode is off)
+echo "${PASSWORD}"
+
+exit 0
